e70cb1c906170dd7c72c5086573a41bf15673038
barrettbrantingham <barrettbrantingham@gmail.com>
2020-03-30 05:15:48 -0500

    I believe everything runs correctly.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -90,7 +90,7 @@ public class HomeController {
             return "redirect:../";
         }
 
-//        return "view";
+
     }
 
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -2,18 +2,26 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
-    @Size(max = 1000, message = "Keep skill description under 1000 characters!")
+    @NotNull(message="Description needed")
     private String description;
 
     @ManyToMany(mappedBy = "skills")
-    private List<Job> jobs = new ArrayList<>();
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Skill( String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
     public String getDescription() {
         return description;
@@ -23,6 +31,6 @@ public class Skill extends AbstractEntity {
         this.description = description;
     }
 
-    public Skill (){}
-
+    public Skill() {
+    }
 }
